5c853930cacc34a8349d8f80cd286aeaaf4cf96e
Sunfei <sunfei.sun@huawei.com>
2022-09-22 22:50:36 +0800

    fix ios build for libuv upgrade
    Signed-off-by: Sunfei <sunfei.sun@huawei.com>
    Change-Id: Ia27852afd3baa30ad199e08c771b3acc659dd2be

--- src/unix/process.c
+++ src/unix/process.c
@@ -387,7 +387,7 @@ static void uv__process_child_init(const uv_process_options_t* options,
 #endif
 
 
-#if defined(__APPLE__)
+#if defined(__APPLE__) && !TARGET_OS_IPHONE
 typedef struct uv__posix_spawn_fncs_tag {
   struct {
     int (*addchdir_np)(const posix_spawn_file_actions_t *, const char *);
@@ -842,7 +842,7 @@ static int uv__spawn_and_init_child(
   int exec_errorno;
   ssize_t r;
 
-#if defined(__APPLE__)
+#if defined(__APPLE__) && !TARGET_OS_IPHONE
   uv_once(&posix_spawn_init_once, uv__spawn_init_posix_spawn);
 
   /* Special child process spawn case for macOS Big Sur (11.0) onwards
