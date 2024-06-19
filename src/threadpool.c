276cc4dea982b7304e95e50c9d0bbe08956762aa
robotsl <liaoxingbin@h-partners.com>
2024-06-19 10:33:37 +0800

    增加uv__work*判空逻辑
    Signed-off-by: robotsl <liaoxingbin@h-partners.com>

--- src/threadpool.c
+++ src/threadpool.c
@@ -716,7 +716,10 @@ static void uv__queue_work(struct uv__work* w) {
 
 static void uv__queue_done(struct uv__work* w, int err) {
   uv_work_t* req;
-
+  if (w == NULL){
+    UV_LOGE("uv__work* w is NULL, returned and unable to call after_work_cb.");
+    return;
+  }
   req = container_of(w, uv_work_t, work_req);
   uv__req_unregister(req->loop, req);
 
