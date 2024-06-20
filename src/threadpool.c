8869669d01536b6257c523f126fe78005dd965be
robotsl <liaoxingbin@h-partners.com>
2024-06-20 15:54:51 +0800

    修改log信息
    Signed-off-by: robotsl <liaoxingbin@h-partners.com>

--- src/threadpool.c
+++ src/threadpool.c
@@ -716,8 +716,9 @@ static void uv__queue_work(struct uv__work* w) {
 
 static void uv__queue_done(struct uv__work* w, int err) {
   uv_work_t* req;
+
   if (w == NULL){
-    UV_LOGE("uv__work* w is NULL, returned and unable to call after_work_cb.");
+    UV_LOGE("uv_work_t is NULL");
     return;
   }
   req = container_of(w, uv_work_t, work_req);
